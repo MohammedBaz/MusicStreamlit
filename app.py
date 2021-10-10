6f3b63fa80fa60626f8e2e70a63e153824e47c87
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-10 18:59:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ if uploaded_file is not None:
   Trainingdataset=Prediction(Trainingdataset=Trainingdataset,modelname='StreamlitModel.h5',TrainingStep=1,PredicitonHorizontal=1)
   #Generate midi file from the results:
   def GenerateMidFile(OriginalandResult):
-  midi = pretty_midi.PrettyMIDI() #define a new mid instance
-  instrument = pretty_midi.Instrument(0) #use a single instrument for this instrument
-  midi.instruments.append(instrument) #add this instrument to the mid file 
-  for pitch in range(len(OriginalandResult)):
-    notex = pretty_midi.Note(velocity=127,pitch=pitch,start=i, end=i+1)
-    instrument.notes.append(notex)
-  return(midi)
+    midi = pretty_midi.PrettyMIDI() #define a new mid instance
+    instrument = pretty_midi.Instrument(0) #use a single instrument for this instrument
+    midi.instruments.append(instrument) #add this instrument to the mid file 
+    for pitch in range(len(OriginalandResult)):
+      notex = pretty_midi.Note(velocity=127,pitch=pitch,start=i, end=i+1)
+      instrument.notes.append(notex)
+    return(midi)
   GenerateMidFile(Trainingdataset+Trainingdataset)
