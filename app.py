18b90188693d9bf6994c9a8ff8535752d3ee53fe
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-10 18:56:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,14 @@ if uploaded_file is not None:
     if not instrument.is_drum:
         for note in instrument.notes:
             Trainingdataset.append(note.pitch)
-  result=Prediction(Trainingdataset=Trainingdataset,modelname='StreamlitModel.h5',TrainingStep=1,PredicitonHorizontal=1)
-  st.write(result)
-  st.write("## Audio file example")
-  audio_file = open(result, "rb")
-  st.audio(audio_file.read())
+  Trainingdataset=Prediction(Trainingdataset=Trainingdataset,modelname='StreamlitModel.h5',TrainingStep=1,PredicitonHorizontal=1)
+  #Generate midi file from the results:
+  def GenerateMidFile(OriginalandResult):
+  midi = pretty_midi.PrettyMIDI() #define a new mid instance
+  instrument = pretty_midi.Instrument(0) #use a single instrument for this instrument
+  midi.instruments.append(instrument) #add this instrument to the mid file 
+  for pitch in range(len(OriginalandResult)):
+    notex = pretty_midi.Note(velocity=127,pitch=pitch,start=i, end=i+1)
+    instrument.notes.append(notex)
+  return(midi)
+  GenerateMidFile(Trainingdataset+Trainingdataset)
