88b266b080689f8852b6290bb682c76c89e39af3
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-10 19:31:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ from BackEndPrediction import Prediction
 Trainingdataset=[]
 uploaded_file = st.file_uploader("upload your mid file or leave blank for random startup", type="mid") #Add file uploader to take the user's input. limited to .png files only
 
+def GenerateMidFile(OriginalandResult):
+  midi = pretty_midi.PrettyMIDI() #define a new mid instance
+  instrument = pretty_midi.Instrument(0) #use a single instrument for this instrument
+  midi.instruments.append(instrument) #add this instrument to the mid file 
+  for i in range(len(OriginalandResult)):
+    notex = pretty_midi.Note(velocity=127,pitch=OriginalandResult[i],start=i, end=i+1)
+    instrument.notes.append(notex)
+  return(midi)
+
+
 if uploaded_file is not None:
   midi_file = uploaded_file
   midi_data = pretty_midi.PrettyMIDI(midi_file)
@@ -16,17 +26,9 @@ if uploaded_file is not None:
     if not instrument.is_drum:
         for note in instrument.notes:
             Trainingdataset.append(note.pitch)
-  results=Prediction(Trainingdataset=Trainingdataset,modelname='StreamlitModel.h5',TrainingStep=1,PredicitonHorizontal=1)
-#Generate midi file from the results:
-def GenerateMidFile(OriginalandResult):
-  midi = pretty_midi.PrettyMIDI() #define a new mid instance
-  instrument = pretty_midi.Instrument(0) #use a single instrument for this instrument
-  midi.instruments.append(instrument) #add this instrument to the mid file 
-  for i in range(len(OriginalandResult)):
-    notex = pretty_midi.Note(velocity=127,pitch=OriginalandResult[i],start=i, end=i+1)
-    instrument.notes.append(notex)
-  return(midi)
-NewMid=GenerateMidFile(results+Trainingdataset)
+  results=Prediction(Trainingdataset=Trainingdataset,modelname='StreamlitModel.h5',TrainingStep=1,PredicitonHorizontal=1) 
+  #Generate midi file from the results:
+  NewMid=GenerateMidFile(results+Trainingdataset)
 
 with st.spinner(f"Transcribing to FluidSynth"):
         midi_data = pretty_midi.PrettyMIDI(NewMid)
