b589b6b2b7fb19e12e42e68d340f9a154e1a7293
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 02:36:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -66,12 +66,12 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
 with st.sidebar.expander("How to prefer to strat with:"):
   add_selectbox=st.sidebar.radio("How to prefer to strat with:", ("Load some audio files", "Use some random Notes", "Use pretrainned Audios"))
   if(add_selectbox=="Load some audio files"):
-  uploaded_file = st.sidebar.file_uploader("Uplod AudioFile Here or leave it blank for random starting", type=['wav','mp3','mid'], accept_multiple_files=False, key=123456) 
-  if uploaded_file is not None:                               # Just to check that the user has its own input to the filed_uploader
-    if not (uploaded_file.name.endswith('mid')):              # if the file is not mid, i.e., it is .wav or.mp3 then
-      FileLocation=StoretheUpoldedFile(uploaded_file)         # Store the file and get its location information 
-      FileType=FileLocation.split(".")[-1]
-      PlayBackMusicFile(FileLocation,FileLocation.split(".")[-1]) # pass the locaiona and extension to PlayBackMusicFile to replay its contents
+    uploaded_file = st.sidebar.file_uploader("Uplod AudioFile Here or leave it blank for random starting", type=['wav','mp3','mid'], accept_multiple_files=False, key=123456) 
+    if uploaded_file is not None:                               # Just to check that the user has its own input to the filed_uploader
+      if not (uploaded_file.name.endswith('mid')):              # if the file is not mid, i.e., it is .wav or.mp3 then
+        FileLocation=StoretheUpoldedFile(uploaded_file)         # Store the file and get its location information 
+        FileType=FileLocation.split(".")[-1]
+        PlayBackMusicFile(FileLocation,FileLocation.split(".")[-1]) # pass the locaiona and extension to PlayBackMusicFile to replay its contents
 with st.sidebar.expander("Add your personal touch, if wish:"):
   PredictionHorizontal = st.sidebar.number_input("Select the Prediction Horizonal, in seconds",min_value=60, max_value =300,value=120,step=10)
   st.sidebar.slider("Select the Inputshape, in sec",0,300,120) #here should be changed in accordnace with the inputs 
