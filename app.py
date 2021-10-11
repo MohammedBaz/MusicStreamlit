ac6dde483c872d91c485978ebd76fbd90c02e4ad
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-11 11:06:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,29 @@ import os
 import base64
 from typing import List
 
+
+def UploadedFileHandler(uploaded_file):
+    if uploaded_file is not None:
+        if uploaded_file.name.endswith('wav'):
+            audio = uploaded_file.read()
+            fileType=('wav')
+        elif uploaded_file.name.endswith('mp3'):
+            audio = uploaded_file.read()
+            fileType=('mp3')
+        else:
+            audio = pretty_midi.PrettyMIDI(uploaded_file)
+            fileType=('mid')         
+   return(audio,fileType)
+
 st.title("AIMC")
 st.header("Artificial Intelligence Music Composer")
 from BackEndPrediction import Prediction
 Trainingdataset=[]
-#uploaded_file = st.file_uploader("upload your mid file or leave blank for random startup", type="mid") #Add file uploader to take the user's input. limited to .png files only
-
-import pydub
-from pathlib import Path
 uploaded_file = st.file_uploader("upload", type=['wav'], accept_multiple_files=False, key=123456)
-if uploaded_file is not None:
-    audio = uploaded_file.read()
-    st.audio(audio, format='audio/wav')
+UploadedFileHandler(uploaded_file)
+st.write("FileUploaded",UploadedFileHandler(uploaded_file)[1])
+
+
+    
+  #  st.audio(audio, format='audio/wav')
 
