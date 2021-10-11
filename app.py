00b1644591ea885960618e5603c88f5971fabe30
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-11 10:31:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,32 +14,11 @@ Trainingdataset=[]
 
 import pydub
 from pathlib import Path
+uploaded_files = st.file_uploader("upload", type=['wav'], accept_multiple_files=False, key=123456)
+if uploaded_file is not None:
+    audio = pydub.AudioSegment.from_wav(uploaded_file)
+    audio.export("/temp/path/file.wav", format="wav")
+    audio_bytes = open("/temp/path/file.wav, 'rb').read()
 
-def upload_and_save_wavfiles(save_dir: str) -> List[Path]:
-    """ limited 200MB, you could increase by `streamlit run foo.py --server.maxUploadSize=1024` """
-    uploaded_files = st.file_uploader("upload", type=['wav', 'mp3'], accept_multiple_files=True, key=123456)
-    save_paths = []
-    for uploaded_file in uploaded_files:
-        if uploaded_file is not None:
-            if uploaded_file.name.endswith('wav'):
-                audio = pydub.AudioSegment.from_wav(uploaded_file)
-                file_type = 'wav'
-            elif uploaded_file.name.endswith('mp3'):
-                audio = pydub.AudioSegment.from_mp3(uploaded_file)
-                file_type = 'mp3'
+    st.audio(audio_bytes, format=f'audio/wav', start_time=0)
 
-            save_path = Path(save_dir) / uploaded_file.name
-            save_paths.append(save_path)
-            audio.export(save_path, format=file_type)
-    return save_paths
-
-def display_wavfile(wavpath):
-    audio_bytes = open(wavpath, 'rb').read()
-    file_type = Path(wavpath).suffix
-    st.audio(audio_bytes, format=f'audio/{file_type}', start_time=0)
-
-
-files = upload_and_save_wavfiles('temp')
-
-for wavpath in files:
-    display_wavfile(wavpath)
