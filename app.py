ecb480d793741ebf934bfa720d07db8e622424bf
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-11 11:20:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,28 +6,22 @@ import os
 import base64
 from typing import List
 
-
-def UploadedFileHandler(uploaded_file):
-  if uploaded_file is not None:
-    if uploaded_file.name.endswith('wav'):
-      audio = uploaded_file.read()
-      fileType=('wav')
-    elif uploaded_file.name.endswith('mp3'):
-      audio = uploaded_file.read()
-      fileType=('mp3')
-    elif uploaded_file.name.endswith('mid'):
-      #audio = pretty_midi.PrettyMIDI(uploaded_file)
-      fileType=('mid')         
-  return (audio,fileType)
-
 st.title("AIMC")
 st.header("Artificial Intelligence Music Composer")
-from BackEndPrediction import Prediction
+
 uploaded_file = st.file_uploader("upload", type=['wav','mp3','mid'], accept_multiple_files=False, key=123456)
-UploadedFileHandler(uploaded_file)
-st.write("FileUploaded",UploadedFileHandler(uploaded_file)[1])
-
-
+if uploaded_file is not None:
+  if uploaded_file.name.endswith('wav'):
+    audio = uploaded_file.read()
+    fileType=('wav')
+    st.audio(audio, format='audio/wav')
+  elif uploaded_file.name.endswith('mp3'):
+    audio = uploaded_file.read()
+    fileType=('mp3')
+    st.audio(audio, format='audio/mp3')
+  elif uploaded_file.name.endswith('mid'):
+    audio = pretty_midi.PrettyMIDI(uploaded_file)
+    fileType=('mid')
+    st.write("replay of mid is not supported currently")
     
-  #  st.audio(audio, format='audio/wav')
 
