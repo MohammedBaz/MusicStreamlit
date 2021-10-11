42596d45ed9931513356e2208f162d414968f719
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 02:39:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
 # Create new file uploader instance and let it accept audio files
 #add_selectbox = st.sidebar.selectbox("How to prefer to strat with",("Load some audio files", "Use some random Notes", "Use pretrainned Audios"))
 with st.sidebar.expander("How to prefer to strat with:"):
-  add_selectbox=st.sidebar.radio("How to prefer to strat with:", ("Load some audio files", "Use some random Notes", "Use pretrainned Audios"))
+  add_selectbox=st.radio("How to prefer to strat with:", ("Load some audio files", "Use some random Notes", "Use pretrainned Audios"))
   if(add_selectbox=="Load some audio files"):
     uploaded_file = st.sidebar.file_uploader("Uplod AudioFile Here or leave it blank for random starting", type=['wav','mp3','mid'], accept_multiple_files=False, key=123456) 
     if uploaded_file is not None:                               # Just to check that the user has its own input to the filed_uploader
@@ -73,10 +73,10 @@ with st.sidebar.expander("How to prefer to strat with:"):
         FileType=FileLocation.split(".")[-1]
         PlayBackMusicFile(FileLocation,FileLocation.split(".")[-1]) # pass the locaiona and extension to PlayBackMusicFile to replay its contents
 with st.sidebar.expander("Add your personal touch, if wish:"):
-  PredictionHorizontal = st.sidebar.number_input("Select the Prediction Horizonal, in seconds",min_value=60, max_value =300,value=120,step=10)
-  st.sidebar.slider("Select the Inputshape, in sec",0,300,120) #here should be changed in accordnace with the inputs 
+  PredictionHorizontal = st.number_input("Select the Prediction Horizonal, in seconds",min_value=60, max_value =300,value=120,step=10)
+  st.slider("Select the Inputshape, in sec",0,300,120) #here should be changed in accordnace with the inputs 
 with st.sidebar.expander("Takeout art piece"):
-  st.sidebar.download_button(label="DOWNLOAD!",data="trees",file_name=FileLocation,mime='audio/.'+FileType)
+  st.download_button(label="DOWNLOAD!",data="trees",file_name=FileLocation,mime='audio/.'+FileType)
   
 
 
