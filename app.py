5bd22da7abe02123b2cc9a261fc6a79a9a912df0
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-11 11:25:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,27 @@ import pandas
 import os
 import base64
 from typing import List
+import wave
 
 st.title("AIMC")
 st.header("Artificial Intelligence Music Composer")
+
+def GetParametersofWav(wavfile=filenamewav):
+  WaveFile=wave.open(wavfile, "rb")
+  n_channels = wav_file.getnchannels()      # Number of channels. (1=Mono, 2=Stereo).
+  sample_width = wav_file.getsampwidth()    # Sample width in bytes.
+  framerate = wav_file.getframerate()       # Frame rate.
+  n_frames = wav_file.getnframes()          # Number of frames.
+  comp_type = wav_file.getcomptype()        # Compression type (only supports "NONE").
+  comp_name = wav_file.getcompname()        # Compression name.
+  st.write(n_channels)
+  st.write(sample_width)
+  st.write(framerate)
+  st.write(n_frames)
+  st.write(comp_type)
+  st.write(comp_name)
+
+
 
 uploaded_file = st.file_uploader("upload", type=['wav','mp3','mid'], accept_multiple_files=False, key=123456)
 if uploaded_file is not None:
@@ -15,6 +33,7 @@ if uploaded_file is not None:
     audio = uploaded_file.read()
     fileType=('wav')
     st.audio(audio, format='audio/wav')
+    GetParametersofWav(audio)
   elif uploaded_file.name.endswith('mp3'):
     audio = uploaded_file.read()
     fileType=('mp3')
@@ -23,5 +42,7 @@ if uploaded_file is not None:
     audio = pretty_midi.PrettyMIDI(uploaded_file)
     fileType=('mid')
     st.write("replay of mid is not supported currently")
+ 
+  
     
 
