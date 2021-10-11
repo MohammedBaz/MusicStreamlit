6d2a81042d31724b5e0c54344c488f11b76120f0
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-11 11:09:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+!pip install streamlit
 import streamlit as st
 import pretty_midi
 import numpy as np 
@@ -8,23 +9,22 @@ from typing import List
 
 
 def UploadedFileHandler(uploaded_file):
-    if uploaded_file is not None:
-        if uploaded_file.name.endswith('wav'):
-            audio = uploaded_file.read()
-            fileType=('wav')
-        elif uploaded_file.name.endswith('mp3'):
-            audio = uploaded_file.read()
-            fileType=('mp3')
-        else:
-            audio = pretty_midi.PrettyMIDI(uploaded_file)
-            fileType=('mid')         
-   return(audio,fileType)
+  if uploaded_file is not None:
+    if uploaded_file.name.endswith('wav'):
+      audio = uploaded_file.read()
+      fileType=('wav')
+    elif uploaded_file.name.endswith('mp3'):
+      audio = uploaded_file.read()
+      fileType=('mp3')
+    else:
+      audio = pretty_midi.PrettyMIDI(uploaded_file)
+      fileType=('mid')         
+  return (audio,fileType)
 
 st.title("AIMC")
 st.header("Artificial Intelligence Music Composer")
 from BackEndPrediction import Prediction
-Trainingdataset=[]
-uploaded_file = st.file_uploader("upload", type=['wav'], accept_multiple_files=False, key=123456)
+uploaded_file = st.file_uploader("upload", type=['wav','mp3','mid'], accept_multiple_files=False, key=123456)
 UploadedFileHandler(uploaded_file)
 st.write("FileUploaded",UploadedFileHandler(uploaded_file)[1])
 
