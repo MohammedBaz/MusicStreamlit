bc0000aee48d944f90d4c36760254895028eda46
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 19:53:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,17 +30,21 @@ def IncreasePageNumber():
         st.session_state.PageNumebr='2'
     elif(st.session_state.PageNumebr=='2'):
         st.session_state.PageNumebr='3'
+    elif(st.session_state.PageNumebr=='3'):
+        st.session_state.PageNumebr='4'    
     else:
-        st.session_state.PageNumebr='None'
+        st.session_state.PageNumebr='PositiveInf'
     st.write(st.session_state.PageNumebr)
     
 def DecreasePageNumber():
-    if  (st.session_state.PageNumebr=='3'):
+    if  (st.session_state.PageNumebr=='4'):
+        st.session_state.PageNumebr='3'
+    elif(st.session_state.PageNumebr=='3'):
         st.session_state.PageNumebr='2'
     elif(st.session_state.PageNumebr=='2'):
-        st.session_state.PageNumebr='1'
+        st.session_state.PageNumebr='1'    
     else:
-        st.session_state.PageNumebr='None'
+        st.session_state.PageNumebr='NegtiveInf'
     st.write(st.session_state.PageNumebr)    
             
             
