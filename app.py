400427a1844639c3492a212444c2bdaefb837738
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-13 00:44:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,15 @@ if(go):
     st.session_state.PageNumebr='2'
     SubtitleofThePage.write("The fist step is to listen to you")
     add_selectbox=DescriptionofThePage.radio("you can upload your audio samples or use ours:",
-                                             ("Upload some audio files", "Use some random Notes", "Use pretrainned Audios"),index=1)    
+                                             ("Upload some audio files", "Use some random Notes", "Use pretrainned Audios"),index=1)
+    if(add_selectbox=="Upload some audio files"):       
+        uploaded_file = DescriptionofThePage.file_uploader("Uplod AudioFile Here or leave it blank if other options are selected",
+                                     type=['wav','mp3','mid'], accept_multiple_files=False, key=123456) 
+        if uploaded_file is not None:                               # Just to check that the user has its own input to the filed_uploader
+            if (uploaded_file.name.endswith('wav')):              # if the file is not mid, i.e., it is .wav or.mp3 then
+                FileLocation=StoretheUpoldedFile(uploaded_file)         # Store the file and get its location information 
+                FileType=FileLocation.split(".")[-1]
+                PlayBackMusicFile(FileLocation,FileLocation.split(".")[-1]) # pass the locaiona and extension to PlayBackMusicFile to replay its contents
     with PrevoiusNextFooter:
         col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
         col10.button('Next',key="MoveToAnalysis")
