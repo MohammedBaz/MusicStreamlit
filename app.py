49d6fdafcd5e6ffc8eb3dd0b3946ef504f74f1ba
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 22:32:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def WelcomPage():
          """)
     with PrevoiusNextFooter:
         col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
-        col10.button('Give a try !',key="next", on_click=IncreasePageNumber)
+        col10.button('Let us Go!',key="next", on_click=IncreasePageNumber)
                                   
 def IncreasePageNumber():
     if  (st.session_state.PageNumebr=='1'):
