eb6a16ac6ad15de7e400503b0471b4b48b56a456
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 23:05:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def DecreasePageNumber():
 def InitalMove():
     TakeUserInput()
     
-WelcomPage() 
+IncreasePageNumber()
                                   
 #with PrevoiusNextFooter:
 #    col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
