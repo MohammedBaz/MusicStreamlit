1d87aa2afb506cab81555ecb43fc6111cc184932
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 23:58:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,7 @@ DescriptionofThePage=st.empty()
 PrevoiusNextFooter=st.empty()
 if 'PageNumebr' not in st.session_state:
     st.session_state['PageNumebr'] = '1'
-
-SubtitleofThePage.write("Warm Welcome from our app!")
-DescriptionofThePage.write("""This is a beta version for an ambitious project aiming to promote the interactivity of 
-          generate some musical pieces using Artificial Intelligence (AI) algorithms.
-         Several AI models have been built and trained to match the variety of musical inputs ; furthermore, 
-         the interface has been optimised to allow a user to add personal toutches and then to download the cratfed musical peices. 
-         Wish this can aid the users to recogenise the potential rules that AI can play in art making. Source code is at:
-         https://github.com/MohammedBaz/MusicStreamlit/blob/main/BackEndPrediction.py  
-         """)
-with PrevoiusNextFooter:
-    col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
-    col10.button('Go!',key="Go", on_click=InitalMove())
+    
     
 def InitalMove():
     st.session_state.PageNumebr='2'
@@ -30,4 +19,18 @@ def InitalMove():
         
         #col10.button('Next',key="next", on_click=IncreasePageNumber)
         #col1.button('Prevoius',key="Prevoius", on_click=DecreasePageNumber)  
+        
+
+SubtitleofThePage.write("Warm Welcome from our app!")
+DescriptionofThePage.write("""This is a beta version for an ambitious project aiming to promote the interactivity of 
+          generate some musical pieces using Artificial Intelligence (AI) algorithms.
+         Several AI models have been built and trained to match the variety of musical inputs ; furthermore, 
+         the interface has been optimised to allow a user to add personal toutches and then to download the cratfed musical peices. 
+         Wish this can aid the users to recogenise the potential rules that AI can play in art making. Source code is at:
+         https://github.com/MohammedBaz/MusicStreamlit/blob/main/BackEndPrediction.py  
+         """)
+with PrevoiusNextFooter:
+    col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
+    col10.button('Go!',key="Go", on_click=InitalMove)
     
+
