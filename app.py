aa50d108c8164ffc6f5624b62a1c03edd93dfe81
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 23:17:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def WelcomPage(SubtitleofThePage,DescriptionofThePage,PrevoiusNextFooter):
     with PrevoiusNextFooter:
         col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
         col10.button('Go!',key="Go", on_click=InitalMove)
-   return(SubtitleofThePage,DescriptionofThePage,PrevoiusNextFooter)
+    return(SubtitleofThePage,DescriptionofThePage,PrevoiusNextFooter)
 
 
 def TakeUserInput():
