57abc168173995ab0c33b7f50856e43b0b7bbde4
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 19:45:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,15 @@ def IncreasePageNumber():
     else:
         st.session_state.PageNumebr='None'
     st.write(st.session_state.PageNumebr)
+    
+def DecreasePageNumber():
+    if  (st.session_state.PageNumebr=='3'):
+        st.session_state.PageNumebr='2'
+    elif(st.session_state.PageNumebr=='2'):
+        st.session_state.PageNumebr='1'
+    else:
+        st.session_state.PageNumebr='None'
+    st.write(st.session_state.PageNumebr)    
             
             
 col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
