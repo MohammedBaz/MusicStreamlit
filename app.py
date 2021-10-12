eacd0aaec846656e2a53361100d3fd1b0bb546ac
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 10:36:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import wave
 
 st.title("AIMC")
 st.header("Interactive Music Composition Using Transfer Learning and GAN")
-st.write('''TThis app can be used to generate some musical pieces using principles of Artificial Intelligence (AI) algorithms. 
-         Several AI models have been built and trained to match the variety of musical inputs; furthermore, 
-         the interface has been optimised to allow a user to add personal toutches and then to download the cratfed musical peices. 
-         Wish this can 
-         ''')
+st.write("TThis app can be used to generate some musical pieces using principles of Artificial Intelligence (AI) algorithms.") 
+#         Several AI models have been built and trained to match the variety of musical inputs; furthermore, 
+#         the interface has been optimised to allow a user to add personal toutches and then to download the cratfed musical peices. 
+#         Wish this can 
+#         ''')
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
