28ee1a85b8293b276264dd8982dc387027782fff
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-13 01:57:06 +0300

    Update MidiFeatures.py

--- MidiFeatures.py
+++ MidiFeatures.py
@@ -1,4 +1,7 @@
 import music21
+from random import randint, choice
+import music21
+
 
 def GetPossibleDuration():
   PossibleDuration=[4.0,3.0,3.5,2.0,4.0/3.0,1.0,2.0/3.0,1.5,1.75,0.5,0.75,1.0/3.0,
