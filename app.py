b463a6015f33dd86a59d4ea9c9ecee88fde2dfc9
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-12 17:03:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,7 @@ if(add_selectbox=="Upload some audio files"):
             FileType=FileLocation.split(".")[-1]
             #PlayBackMusicFile(FileLocation,FileLocation.split(".")[-1])
 
-st.button('Next')
+col1, col2, col3 = st.beta_columns(3)
+if col2.button('Click'):
+    st.write('hello')
 
