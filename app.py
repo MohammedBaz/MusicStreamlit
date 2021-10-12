f2d4e496c665dbbfcf0ad0537691385c9d2a7807
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-13 00:32:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ DescriptionofThePage.write("""This is a beta version for an ambitious project ai
 with PrevoiusNextFooter:
     col1, col2, col3,col4, col5, col6,col7, col8, col9,col10 = st.columns(10)
     go=col10.button('Go',key="go")
-    if(go):
-        st.write("fuck")
+
+if(go):
+    st.write("fuck")
 
 
