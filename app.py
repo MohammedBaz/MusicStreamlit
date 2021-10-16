4be7be03c00f582c069e40c3aa5186c82e7daf85
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-16 13:06:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ def PlotTempoChanges(InputFile):
 def PlotPitchDistribution(InputFile):
   pm= pretty_midi.PrettyMIDI(InputFile)
   fig, ax = plt.subplots()
-  plt.bar(np.arange(12), pm.get_pitch_class_histogram());
-  xticksTitle=['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B']
+  plt.bar(numoy.arange(12), pm.get_pitch_class_histogram());
+  
   #ax.set_xticks(xticksTitle)
   ax.set_xticklabels(['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B'])
   #fig.xlabel('Note')
