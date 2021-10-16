c247ba85bce83ef92bdd504ef46f73e1603714de
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-16 12:24:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -139,12 +139,12 @@ def PlotPitchDistribution(InputFile):
   pm= pretty_midi.PrettyMIDI(InputFile)
   fig, ax = plt.subplots()
   ax.bar(numpy.arange(12), pm.get_pitch_class_histogram());
-  #ax.xticks(numpy.arange(12), ['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B'])
-  #ax.xlabel('Note')
-  #ax.ylabel('Proportion')
+  fig.xticks(numpy.arange(12), ['C', '', 'D', '', 'E', 'F', '', 'G', '', 'A', '', 'B'])
+  fig.xlabel('Note')
+  fig.ylabel('Proportion')
   st.pyplot(fig)
   
-  
+ 
   
   
   
