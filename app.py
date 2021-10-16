0b2e20491280f4e52af078230a396c6d1bb276eb
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-16 11:08:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ if(add_selectbox=="Upload your audio files"):
   uploaded_file = MainPageDescription.file_uploader("Uplod AudioFile Here or leave it blank if other options are selected",type=['mid'], accept_multiple_files=False) 
   if uploaded_file is not None:                              # Just to check that the user has its own input to the filed_uploader
     FileLocation=StoretheUpoldedFile(uploaded_file)
-    DisplayGeneralFeatrues(uploaded_file)
+    DisplayGeneralFeatrues(FileLocation)
     with st.expander("Musical Sheet"):
       musictrack=music21.converter.parse(FileLocation)
       DisplayMusicalNotes(musictrack)
