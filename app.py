36ba9191e9ea9d768610ea687cf2878e06f6a0cc
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-16 14:13:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ with st.sidebar.expander("The first step is listen to you"):
       if uploaded_file is not None:                              # Just to check that the user has its own input to the filed_uploader
         FileLocation=StoretheUpoldedFile(uploaded_file)
         a,b,c= DisplayGeneralFeatrues(FileLocation)
-        MainPageDescription.write(a)
+        MainPageDescription.write("It is interesting truck of"+str(a))
         
         #MainPageDescription.write('It is interesting truck of'+a+ 'second'+
         #                          'It consists of'+b+'notes'+ 
