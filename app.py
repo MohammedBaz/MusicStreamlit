97134a5429bc0b1ed2ef4cfb173b0728b7103c10
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-16 13:21:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -162,18 +162,17 @@ MainPageDescription.write("""This is a beta version for an ambitious project aim
 
 with st.sidebar.expander("The first step is listen to you"):
     MainPageDescription.empty() 
-    add_selectbox=st.radio("Here you can:", ("Upload your audio files", "Generate musical Notes", "Use our pregeneraed Audios"),index=1)
-if(add_selectbox=="Upload your audio files"):
-  MainPageDescription.empty()       
-  uploaded_file = MainPageDescription.file_uploader("Uplod AudioFile Here or leave it blank if other options are selected",type=['mid'], accept_multiple_files=False) 
-  if uploaded_file is not None:                              # Just to check that the user has its own input to the filed_uploader
-    FileLocation=StoretheUpoldedFile(uploaded_file)
-    DisplayGeneralFeatrues(FileLocation)
-    with st.expander("Plot Tempo Changes"):
+    add_selectbox=st.radio("We can aid you to compete your piece: ", ("Upload your audio files", "Generate musical Notes", "Use our pregeneraed Audios"),index=1)
+    if(add_selectbox=="Upload your audio files"):
+      uploaded_file = MainPageDescription.file_uploader("Uplod AudioFile Here or leave it blank if other options are selected",type=['mid'], accept_multiple_files=False) 
+      if uploaded_file is not None:                              # Just to check that the user has its own input to the filed_uploader
+      FileLocation=StoretheUpoldedFile(uploaded_file)
+      DisplayGeneralFeatrues(FileLocation)
+with st.expander("Plot Tempo Changes"):
       PlotTempoChanges(FileLocation)
-    with st.expander("Plot Pitch Distrbution"):   
+with st.expander("Plot Pitch Distrbution"):   
       PlotPitchDistribution(FileLocation)
-    with st.expander("Musical Sheet"):
+with st.expander("Musical Sheet"):
       musictrack=music21.converter.parse(FileLocation)
       DisplayMusicalNotes(musictrack)
       
