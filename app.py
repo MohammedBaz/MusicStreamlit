94104e464f2c1c9a8a43ccb45474899d06fb0b3d
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-16 09:54:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -144,10 +144,10 @@ if(add_selectbox=="Upload your audio files"):
   if uploaded_file is not None:                              # Just to check that the user has its own input to the filed_uploader
     FileLocation=StoretheUpoldedFile(uploaded_file)
     musictrack=music21.converter.parse(FileLocation)
-    DisplayContents=st.empty():
+    DisplayContents=st.empty()
       with import time:
         import time
-        my_bar = st.progress(0)
+        my_bar = DisplayContents.progress(0)
         for percent_complete in range(100):
           time.sleep(0.1)
           my_bar.progress(percent_complete + 1)
