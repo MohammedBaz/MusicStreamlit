2da679a1980370516960d95c1d8117b6b2bfa414
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-19 02:40:37 +0300

    Update BackEndPrediction.py

--- BackEndPrediction.py
+++ BackEndPrediction.py
@@ -1,3 +1,23 @@
+
+from keras.models import load_model
+import numpy
+
+def Prediction(modelname,Trainingdataset1,Trainingdataset2,TimeStep,PredicitonHorizontal):
+  model =load_model(modelname)
+  LengthofOriginalTrainingdataset=len(Trainingdataset1)
+  Predicitonresults1=numpy.array(Trainingdataset1)
+  Predicitonresults2=numpy.array(Trainingdataset2)
+  for i in range(PredicitonHorizontal):
+    yhat=model.predict([numpy.array(Predicitonresults1[-TimeStep:]).reshape(1,TimeStep,1),
+                        numpy.array(Predicitonresults2[-TimeStep:]).reshape(1,TimeStep,1)])
+    Predicitonresults1=numpy.append(Predicitonresults1, yhat[0])
+    Predicitonresults2=numpy.append(Predicitonresults2, yhat[1])
+  return (Predicitonresults1,Predicitonresults2)
+
+
+
+"""
+
 import keras
 from numpy import array
 
