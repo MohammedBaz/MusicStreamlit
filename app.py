699b5c836244fa902854683d231be757cb249d06
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-18 20:21:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ with st.sidebar.expander("The first step is listen to you"):
         
         PlayBackMusicFile(ConvertMiditoWave(FileLocationofGeneraedMelody))
         PlotPitchDistribution(FileLocationofGeneraedMelody)
-        musictrack=music21.converter.parse(FileLocation,, format='midi',
+        musictrack=music21.converter.parse(FileLocation, format='midi',
 ...                                                  forceSource=True, quantizePost=True)
         st.write(musictrack)
         #DisplayMusicalNotes(musictrack)
