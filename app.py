18d38eb6f4485081e1a03d759b8224b70fbc172f
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2021-10-19 17:39:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -158,6 +158,12 @@ with st.sidebar.expander("Here you can add personalise generation process:"):
         
         OverallAmplitude=numpy.array(Trainingdataset1)/127
         OverallScale=numpy.array(Trainingdataset2)/127
+        
+        if(TimeStep !=2): # if the user attempts to change the timestep from 2 where the model is trained to other values then:
+            ModifiedModel=KerasAPImodel(TimeStep) # create new model with the given 
+            ModifiedModel.set_weights(model.get_weights())
+            model=ModifiedModel
+        
         for i in range(30):
             yhat=model.predict([OverallAmplitude[-TimeStep:].reshape(1,TimeStep,1),
                         OverallScale[-TimeStep:].reshape(1,TimeStep,1)])
